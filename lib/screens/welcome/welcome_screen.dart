2a3aea14a24e5ca72136c15ba3cfb7eceffbcb17
Tarek Faysal <faysal5467@gmail.com>
2021-09-30 16:58:22 +0600

    buttons

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,67 +1,53 @@
 import 'package:flutter/material.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key key}) : super(key: key);
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  Color myColor = Colors.red;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: Icon(Icons.agriculture),
+          icon: Icon(Icons.arrow_back),
           onPressed: () {},
         ),
         backgroundColor: Colors.green,
         title: Text("Our first project"),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                  color: Colors.red,
-                  margin: EdgeInsets.all(20),
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: Text("Hello")),
-              Container(
-                  color: Colors.blue,
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(5),
-                  child:
-                      ElevatedButton(onPressed: () {}, child: Text("Barisal")))
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                  color: Colors.red,
-                  margin: EdgeInsets.all(20),
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: Text("Hello")),
-              Container(
-                  color: Colors.blue,
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(5),
-                  child:
-                      ElevatedButton(onPressed: () {}, child: Text("Barisal")))
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                  color: Colors.red,
-                  margin: EdgeInsets.all(20),
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: Text("Hello")),
-              Container(
-                  color: Colors.blue,
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(5),
-                  child:
-                      ElevatedButton(onPressed: () {}, child: Text("Barisal")))
-            ],
-          ),
-        ],
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green)),
+                onPressed: () {
+                  setState(() {
+                    if (myColor == Colors.red) {
+                      myColor = Colors.blue;
+                    } else if (myColor == Colors.blue) {
+                      myColor = Colors.green;
+                    } else {
+                      myColor = Colors.red;
+                    }
+                  });
+                },
+                child: Text(
+                  "Change Color",
+                  style: TextStyle(color: Colors.white),
+                )),
+            Container(height: 200, width: 200, color: myColor)
+          ],
+        ),
       ),
     );
   }
